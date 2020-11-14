50a2b5f95c1ee482d47edfd91915f882872a80af
Danny Campbell <dtc53@drexel.edu>
2020-11-14 14:38:29 -0500

    added all click upgrades

--- Assets/Scripts/BeanCounter.cs
+++ Assets/Scripts/BeanCounter.cs
@@ -8,6 +8,12 @@ public class BeanCounter : MonoBehaviour
     public Text BeansCountText;
     public Text BeansPerClickText;
 
+    public Text Upgrade1Text;
+    public Text Upgrade2Text;
+    public Text Upgrade3Text;
+    public Text Upgrade4Text;
+    public Text Upgrade5Text;
+
     void OnMouseDown()
     {
         Global.numBeans += Global.beansPerClick;
@@ -16,35 +22,94 @@ public class BeanCounter : MonoBehaviour
 
     public void Upgrade1()
     {
-        //if (Global.numBeans >= Global.upgrade1Cost)
-        //{
-        //    Global.beansPerClick += Global.upgrade1Increase;
-        //    Global.numBeans -= Global.upgrade1Cost;
-        //    BeansCountText.text = "Beans: " + Global.numBeans.ToString();
-        //    BeansPerClickText.text = "BPC: " + Global.beansPerClick.ToString();
-        //}
-        UpgradeHelper(Global.upgrade1Cost, Global.upgrade1Increase);
+        if (Global.upgrade1Cost < Global.upgrade1MaxCost && Global.numBeans >= Global.upgrade1Cost)
+        {
+            UpgradeHelper(Global.upgrade1Cost, Global.upgrade1Increase, Global.upgrade1CostInc, Global.upgrade1MaxCost, Upgrade1Text);
+            Global.upgrade1Cost += Global.upgrade1CostInc;
+            if (Global.upgrade1Cost >= Global.upgrade1MaxCost)
+            {
+                Upgrade1Text.text = "Upgrade Max";
+            }
+            else
+            {
+                Upgrade1Text.text = "Beans Per Click +" + Global.upgrade1Increase + "\nCosts: " + Global.upgrade1Cost + " Beans";
+            }
+        }
     }
 
     public void Upgrade2()
     {
-        if (Global.numBeans >= Global.upgrade2Cost)
+        if (Global.upgrade2Cost < Global.upgrade2MaxCost && Global.numBeans >= Global.upgrade2Cost)
         {
-            Global.beansPerClick += Global.upgrade2Increase;
-            Global.numBeans -= Global.upgrade2Cost;
-            BeansCountText.text = "Beans: " + Global.numBeans.ToString();
-            BeansPerClickText.text = "BPC: " + Global.beansPerClick.ToString();
+            UpgradeHelper(Global.upgrade2Cost, Global.upgrade2Increase, Global.upgrade2CostInc, Global.upgrade2MaxCost, Upgrade2Text);
+            Global.upgrade2Cost += Global.upgrade2CostInc;
+            if (Global.upgrade2Cost >= Global.upgrade2MaxCost)
+            {
+                Upgrade2Text.text = "Upgrade Max";
+            }
+            else
+            {
+                Upgrade2Text.text = "Beans Per Click +" + Global.upgrade2Increase + "\nCosts: " + Global.upgrade2Cost + " Beans";
+            }  
         }
     }
 
-    void UpgradeHelper(int cost, int increase)
+    public void Upgrade3()
     {
-        if (Global.numBeans >= cost)
+        if (Global.upgrade3Cost < Global.upgrade3MaxCost && Global.numBeans >= Global.upgrade3Cost)
         {
-            Global.beansPerClick += increase;
-            Global.numBeans -= cost;
-            BeansCountText.text = "Beans: " + Global.numBeans.ToString();
-            BeansPerClickText.text = "BPC: " + Global.beansPerClick.ToString();
+            UpgradeHelper(Global.upgrade3Cost, Global.upgrade3Increase, Global.upgrade3CostInc, Global.upgrade3MaxCost, Upgrade3Text);
+            Global.upgrade3Cost += Global.upgrade3CostInc;
+            if (Global.upgrade3Cost >= Global.upgrade3MaxCost)
+            {
+                Upgrade3Text.text = "Upgrade Max";
+            }
+            else
+            {
+                Upgrade3Text.text = "Beans Per Click +" + Global.upgrade3Increase + "\nCosts: " + Global.upgrade3Cost + " Beans";
+            }
         }
+    }
+
+    public void Upgrade4()
+    {
+        if (Global.upgrade4Cost < Global.upgrade4MaxCost && Global.numBeans >= Global.upgrade4Cost)
+        {
+            UpgradeHelper(Global.upgrade4Cost, Global.upgrade4Increase, Global.upgrade4CostInc, Global.upgrade4MaxCost, Upgrade4Text);
+            Global.upgrade4Cost += Global.upgrade4CostInc;
+            if (Global.upgrade4Cost >= Global.upgrade4MaxCost)
+            {
+                Upgrade4Text.text = "Upgrade Max";
+            }
+            else
+            {
+                Upgrade4Text.text = "Beans Per Click +" + Global.upgrade4Increase + "\nCosts: " + Global.upgrade4Cost + " Beans";
+            }
+        }
+    }
+
+    public void Upgrade5()
+    {
+        if (Global.upgrade5Cost < Global.upgrade5MaxCost && Global.numBeans >= Global.upgrade5Cost)
+        {
+            UpgradeHelper(Global.upgrade5Cost, Global.upgrade5Increase, Global.upgrade5CostInc, Global.upgrade5MaxCost, Upgrade5Text);
+            Global.upgrade5Cost += Global.upgrade5CostInc;
+            if (Global.upgrade5Cost >= Global.upgrade5MaxCost)
+            {
+                Upgrade5Text.text = "Upgrade Max";
+            }
+            else
+            {
+                Upgrade5Text.text = "Beans Per Click +" + Global.upgrade5Increase + "\nCosts: " + Global.upgrade5Cost + " Beans";
+            }
+        }
+    }
+
+    void UpgradeHelper(int cost, int increase, int costInc, int maxCost, Text text)
+    {
+        Global.beansPerClick += increase;
+        Global.numBeans -= cost;
+        BeansCountText.text = "Beans: " + Global.numBeans.ToString();
+        BeansPerClickText.text = "BPC: " + Global.beansPerClick.ToString();
     }
 }
--- Assets/Scripts/Global.cs
+++ Assets/Scripts/Global.cs
@@ -10,9 +10,31 @@ public class Global : MonoBehaviour
 
     //Upgrade 1
     public static int upgrade1Increase = 1;
-    public static int upgrade1Cost = 10;
+    public static int upgrade1Cost = 50;
+    public static int upgrade1CostInc = 1;
+    public static int upgrade1MaxCost = 100;
 
     //Upgrade 2
     public static int upgrade2Increase = 5;
-    public static int upgrade2Cost = 100;
+    public static int upgrade2Cost = 500;
+    public static int upgrade2CostInc = 5;
+    public static int upgrade2MaxCost = 750;
+
+    //Upgrade 3
+    public static int upgrade3Increase = 20;
+    public static int upgrade3Cost = 2500;
+    public static int upgrade3CostInc = 50;
+    public static int upgrade3MaxCost = 10000;
+
+    //Upgrade 4
+    public static int upgrade4Increase = 50;
+    public static int upgrade4Cost = 10000;
+    public static int upgrade4CostInc = 100;
+    public static int upgrade4MaxCost = 30000;
+
+    //Upgrade 5
+    public static int upgrade5Increase = 100;
+    public static int upgrade5Cost = 50000;
+    public static int upgrade5CostInc = 500;
+    public static int upgrade5MaxCost = 150000;
 }
