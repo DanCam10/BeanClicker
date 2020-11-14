c1909fdb7a95c7a206e72b16a3bfdd9d55062556
Danny Campbell <dtc53@drexel.edu>
2020-11-14 12:26:11 -0500

    added start of project

---
+++ Assets/Scripts/BeanCounter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BeanCounter : MonoBehaviour
+{
+    public Text BeansCountText;
+    public Text BeansPerClickText;
+
+    void OnMouseDown()
+    {
+        Global.numBeans += Global.beansPerClick;
+        BeansCountText.text = "Beans: " + Global.numBeans.ToString();
+    }
+
+    public void Upgrade1()
+    {
+        //if (Global.numBeans >= Global.upgrade1Cost)
+        //{
+        //    Global.beansPerClick += Global.upgrade1Increase;
+        //    Global.numBeans -= Global.upgrade1Cost;
+        //    BeansCountText.text = "Beans: " + Global.numBeans.ToString();
+        //    BeansPerClickText.text = "BPC: " + Global.beansPerClick.ToString();
+        //}
+        UpgradeHelper(Global.upgrade1Cost, Global.upgrade1Increase);
+    }
+
+    public void Upgrade2()
+    {
+        if (Global.numBeans >= Global.upgrade2Cost)
+        {
+            Global.beansPerClick += Global.upgrade2Increase;
+            Global.numBeans -= Global.upgrade2Cost;
+            BeansCountText.text = "Beans: " + Global.numBeans.ToString();
+            BeansPerClickText.text = "BPC: " + Global.beansPerClick.ToString();
+        }
+    }
+
+    void UpgradeHelper(int cost, int increase)
+    {
+        if (Global.numBeans >= cost)
+        {
+            Global.beansPerClick += increase;
+            Global.numBeans -= cost;
+            BeansCountText.text = "Beans: " + Global.numBeans.ToString();
+            BeansPerClickText.text = "BPC: " + Global.beansPerClick.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Global : MonoBehaviour
+{
+    //General Beans
+    public static int numBeans = 0;
+    public static int beansPerClick = 1;
+
+    //Upgrade 1
+    public static int upgrade1Increase = 1;
+    public static int upgrade1Cost = 10;
+
+    //Upgrade 2
+    public static int upgrade2Increase = 5;
+    public static int upgrade2Cost = 100;
+}
